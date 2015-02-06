61d6277481457b25a3c0d25ace2a1eddfe0a5787
Adam Chao <adam@thinkaliker.com>
2015-02-06 00:35:04 -0800

    added folder/file existence checks, moved around prompt logic, squashed bugs

--- logparser.bat
+++ logparser.bat
@@ -13,6 +13,36 @@ echo Created by thinkaliker                                  [http://thinkaliker
 echo Source available on GitHub         [http://github.com/thinkaliker/MCFMIRCParser]
 echo ////////////////////////////////////////////////////////////////////////////////
 
+set cpy=%temp%\lp_cpy.txt
+set mcat=%temp%\lp_mcat.txt
+set line=%temp%\lp_line.txt
+set sa=%temp%\lp_sa.txt
+set satemp=%temp%\lp_satemp.txt
+
+::cleanup any previous sessions
+break>%mcat%
+break>%line%
+break>%sa%
+break>%satemp%
+
+:loc
+set dest=%temp%\lp_np.txt
+set /p destloc="Enter text file output location (Somewhere on C:\ recommended): "
+if /i "%destloc%" EQU "none" (
+	echo You must enter a destination file.
+	GOTO :loc
+) else (
+	set dest=%destloc%
+	if exist %destloc% (
+		GOTO :prompts
+	) else (
+		echo This file does not exist. We will create it for you.
+		fsutil file createnew %destloc% 0 > NUL
+		GOTO :prompts
+	)
+
+)
+
 :prompts
 set username=none
 set /p username="Enter IRC channel name (should be your username): "
@@ -20,22 +50,12 @@ if /i "%username%" EQU "none" (
 	echo You must enter a username.
 	GOTO :prompts
 ) else (
-	GOTO :loc
-)
-
-:loc
-set dest=none
-set /p dest="Enter text file output location (eg. D:\nowplaying.txt ): "
-if /i "%dest%" EQU "none" (
-	echo You must enter a destination file.
-	GOTO :loc
-) else (
 	GOTO :client
 )
 
 :client
 set src=none
-set /p src="Which IRC client are you using? Type [p] for Pidgin or [h] for HexChat: "
+set /p src="Which IRC client? Type [ p ] for Pidgin or [ h ] for HexChat: "
 if /i "%src%" EQU "none" (
 	echo You must enter a client.
 	GOTO :client
@@ -47,24 +67,48 @@ if /i "%src%" EQU "none" (
 if %src%==p GOTO :p
 if %src%==h GOTO :h
 
-echo Invalid input, try again.
-GOTO :prompts
+echo Invalid client, try again. Only Pidgin or HexChat is supported.
+GOTO :client
 
 :p
 :: make sure this source line matches whatever DIRECTORY your chat logs are stored in - please make sure this is correct for your system/configuration
 set source=%AppData%\.purple\logs\irc\%username%@irc.twitch.tv\#%username%.chat
-echo You are using Pidgin.
+if exist %source% (
+	echo You are using Pidgin.
+) else (
+	echo The folder doesn't exist. Did you follow the set up instructions?
+	GOTO :prompts
+)
 GOTO :delay
 
 :h
 :: make sure this source line matches whatever DIRECTORY your chat logs are stored in - please make sure this is correct for your system/configuration
 set source=%AppData%\HexChat\logs\twitch
-echo You are using HexChat.
+if exist %source% (
+	echo You are using HexChat.
+) else (
+	echo The folder doesn't exist. Did you follow the set up instructions?
+	GOTO :prompts
+)
 GOTO :delay
 
 :delay
 set /a delay=10
 set /p delay="Default delay is 10 seconds. Configured delay (optional): "
+
+if %src%==p GOTO :pnew
+if %src%==h GOTO :hnew
+
+:pnew
+for /f "tokens=*" %%G in ('dir *.txt /b /a-d /od %source% 2^> NUL') do (
+	set newest=%%G
+)
+set cpysrc=%source%\%newest%
+GOTO :begin
+
+:hnew
+set cpysrc=%source%\#%username%.log
+GOTO :begin
 
 :begin
 echo --------------------------------------------------------------------------------
@@ -75,38 +119,23 @@ echo First time running may require a !song command in Twitch to show the first 
 echo If no song is shown below, the song has not changed since last run.
 echo ================================================================================
 
-set cpy=%temp%\lp_cpy.txt
-set mcat=%temp%\lp_mcat.txt
-set line=%temp%\lp_line.txt
-set sa=%temp%\lp_sa.txt
-
-:parse
-
+:recopy
 if %src%==p GOTO :pcopy
 if %src%==h GOTO :hcopy
 
 :pcopy
-for /f "tokens=*" %%G in ('dir *.txt /b /a-d /od %source% 2^> NUL') do (
-	set newest=%%G
-)
-copy /V /Y %source%\%newest% %cpy% > NUL
+copy /V /Y %cpysrc% %cpy% > NUL
 GOTO :mcatfind
 
 :hcopy
-copy /V /Y %source%\#%username%.log %cpy% > NUL
+copy /V /Y %cpysrc% %cpy% > NUL
 GOTO :mcatfind
 
 :mcatfind
-break>%mcat%
-break>%line%
-
 findstr ".*monstercat.*.*Now Playing:.*- Listen now: http://spoti.fi/.*" %cpy% > %mcat%
-
 for /f %%C in ('find /V /C "" ^< %mcat%') do set LINES=%%C
-
 set /a LINES=LINES-1
-
-more +%LINES% %mcat% > %line%
+more /S +%LINES%  %mcat% > %line%
 
 ::  _____ ___  ____  __  __    _  _____ _____ ___ _   _  ____ 
 :: |  ___/ _ \|  _ \|  \/  |  / \|_   _|_   _|_ _| \ | |/ ___|
@@ -119,24 +148,23 @@ more +%LINES% %mcat% > %line%
 :: $1 is the song name
 :: $2 is the artist name
 :: simply rearrange to change the order and formatting you desire
-type %line% | jrepl ".*Playing: (.*) by (.*) -.*" "$2 // $1" /O %sa%
-:: replace the above^ %sa% with %dest% and delete this line \/
+type %line% | jrepl ".*Playing: (.*) by (.*) -.*" "$2 // $1" > %sa%
 set /p string= < %sa%
+upper "%string%" | jrepl "\q(.*)\q" "$1" /B /X > %satemp%
 
 ::keep \/
-fc /c %sa% %dest% > NUL && GOTO :nochange || GOTO :change
+fc /A /C %dest% %sa% > NUL && GOTO :nochange || GOTO :change
 ::keep /\
 
 :change
-:: \/ and this ilne if you DO NOT want all caps
-upper "%string%" | jrepl "\q(.*)\q" "$1" /B /X > %dest%
+:: \/ change this ilne if you DO NOT want all caps
+type %satemp% > %dest%
 :: this just shows you what the string you're putting into the OBS text file looks like
 type %dest%
-GOTO :timeout
+GOTO :wait
 
 :nochange
-
-:timeout
+:wait
 timeout /t %delay% /nobreak > NUL
 
-GOTO :parse
+GOTO :recopy
